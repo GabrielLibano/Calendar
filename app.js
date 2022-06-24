5b60a1c1a4f795f8ce3ed1dfb79a059707ab6f9c
GabrielLibano <gabriellibanoanselmo@hotmail.com>
2022-06-24 12:49:34 -0300

    Calendario

--- app.js
+++ app.js
@@ -16,8 +16,30 @@ for(i=0; i<31; i++){
 
 let day = document.querySelectorAll(".day");
 
-day.forEach(days => {
-    days.addEventListener('click', evento => {
-        console.log(evento.currentTarget.classList.toggle("selected"));
-    })
+function selectDay(){
+    day.forEach(days => {
+        days.addEventListener('click', evento => {
+            let currentTarget =  evento.currentTarget.classList.toggle("selected");
+            let selecionado = evento.currentTarget.classList.add('teste');
+        })
+    });
+}
+selectDay();
+day.forEach(days=>{
+    days.addEventListener('click', eventdb => {
+        let comment = document.getElementById("main-comentario");
+        comment.style.display = 'flex';
+    });
 });
+
+function anotacao(){
+    let textAnotacao = document.getElementById("text").value;
+    let selecionado = document.querySelector(".teste");
+    selecionado.innerHTML += `<span>${textAnotacao}</span>`;
+
+    selecionado.classList.remove('teste')
+
+    
+    let comment = document.getElementById("main-comentario");
+    comment.style.display = 'none';
+}
